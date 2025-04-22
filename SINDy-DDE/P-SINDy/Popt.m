52d684b5b5c5d15f4e85a8c8fff1ddd7b024f2d3
MuhammadTanveer1994 <141918952+MuhammadTanveer1994@users.noreply.github.com>
2025-04-22 15:23:01 +0200

    Popt.m

--- SINDy-DDE/P-SINDy/Popt.m
+++ SINDy-DDE/P-SINDy/Popt.m
@@ -1,4 +1,4 @@
-function [g,phi,par,tspan,X_data,DX,Xi,Theta_all,x_sindy,x_sol] = Popt(model,tau,par)
+function [g,phi,par,tspan,X_data,DX,Xi,Theta_all] = Popt(model,tau,par)
 
 if strcmp(model, 'MG')
         [g,~,phi,par0,n] = P_Model(model);
@@ -10,13 +10,13 @@ if strcmp(model, 'MG')
 
     %% Parameters 
     polyorder = 2;   
-    usesine = 0;
+    usesine = 1;
     t_r = 0.6; 
     n_a = 0; 
     T = 30; 
     tspan = [0 T]; 
     N = 100;
-    M = 10;  
+    M = 15;  
 
     dist = 1;  % '(1) Uniform', '(2) Random';
     [X_data, DX, u0, D, t_ae, x_tn] = P_Data(model, g, tau, phi, par, tspan, M, t_r, n_a, N, dist);
@@ -30,24 +30,24 @@ if strcmp(model, 'MG')
 
     Xi = sparsifyDynamics(Theta_all, DX, lambda, alpha, n, method, k);
 
-    options = odeset('RelTol',1e-4,'AbsTol',1e-6);
-
-    bestTau_new = max(abs(tau));
-    [D_new, theta_new] = difmat(-bestTau_new, 0, M);
-    X_new = phi(theta_new);
-    u0_new = X_new;
-    
-    solA = ode15s(@(t,u) d_ODE(t, u, par, tau, M, model, D), tspan, u0, options);
-    
-    if strcmp(model, 'MG')
-       solSINDy = ode15s(@(t,u) gPSINDy(t, u, Xi, polyorder, usesine, model, D_new, par(3)), tspan, u0_new, options);
-    else
-       solSINDy = ode15s(@(t,u) gPSINDy(t, u, Xi, polyorder, usesine, model, D_new), tspan, u0_new, options);
-    end
-
-    t_valid = solSINDy.x;  
-    t_ae = t_ae(t_ae >= t_valid(1) & t_ae <= t_valid(end));  
-    
-    x_sindy = deval(solSINDy, t_ae);
-    x_sol = deval(solA, t_ae);
+    % options = odeset('RelTol',1e-4,'AbsTol',1e-6);
+    % 
+    % bestTau_new = max(abs(tau));
+    % [D_new, theta_new] = difmat(-bestTau_new, 0, M);
+    % X_new = phi(theta_new);
+    % u0_new = X_new;
+    % 
+    % solA = ode15s(@(t,u) d_ODE(t, u, par, tau, M, model, D), tspan, u0, options);
+    % 
+    % if strcmp(model, 'MG')
+    %    solSINDy = ode15s(@(t,u) gPSINDy(t, u, Xi, polyorder, usesine, model, D_new, par(3)), tspan, u0_new, options);
+    % else
+    %    solSINDy = ode15s(@(t,u) gPSINDy(t, u, Xi, polyorder, usesine, model, D_new), tspan, u0_new, options);
+    % end
+    % 
+    % t_valid = solSINDy.x;  
+    % t_ae = t_ae(t_ae >= t_valid(1) & t_ae <= t_valid(end));  
+    % 
+    % x_sindy = deval(solSINDy, t_ae);
+    % x_sol = deval(solA, t_ae);
 end
